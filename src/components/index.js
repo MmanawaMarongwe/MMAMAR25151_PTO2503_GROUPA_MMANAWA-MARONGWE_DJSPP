ed21178e8382219d6b885ebf69c99885ed270681
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2026-02-07 08:59:53 +0200

    Add a theme toggle icon into the app

--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,5 @@ export { default as SeasonList} from "./SeasonList"
 export {default as ShowCover} from "./ShowCover"
 export { default as ShowDetailHeader} from "./ShowDetailHeader"
 export {default as ShowInfo} from "./ShowInfo"
+export {default as GenreTags} from "./GenreTags"
 

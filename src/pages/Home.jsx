48383d669ec19d8e0b9dc8486938da5eacefe45d
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2026-02-10 08:49:15 +0200

    Add recommendations carousel to home page using podcast context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import { PodcastGrid, Filters } from "../components/index";
 import { Pagination } from "../UI/index";
+import RecommendationCarousel from "../components/RecommendationCarousel";
+import { usePodcasts } from "../context/PodcastContext";
+
+
+
 
 export default function Home() {
+   const { podcasts } = usePodcasts();
   return (
     <>
       <Filters />
       <main>
+        <RecommendationCarousel shows={podcasts} />
         <PodcastGrid />
         <Pagination />
       </main>
